595bf5768888ebc91ae926d4c191554402a9340c
Neil McEachin <njmceach@mtu.edu>
2018-01-28 20:21:27 -0500

    Finished protecting requests and hiding links until logged in

--- app.js
+++ app.js
@@ -46,6 +46,10 @@ app.get('/', (req, res) => {
 	res.send('Invalid Endpoint');
 });
 
+app.get('*', () => {
+	res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
 //Start Server
 app.listen(port,() => {
 	console.log('Server started on port '+port);
